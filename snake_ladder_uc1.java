9ad633d6f1fef99039051a5504888e9a2ed5abe0
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-02 21:49:36 +0530

    Added: Snake-ladder-uc1

---
+++ snake_ladder_uc1.java
@@ -0,0 +1,15 @@
+package snake_ladder;
+
+import java.util.Random;
+
+public class snake_ladder_uc1 {
+
+	public static void main(String[] args) {
+			int n=0;
+			
+				System.out.println("player position : "   +n);
+
+		
+	}
+
+}
