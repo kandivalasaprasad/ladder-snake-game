071a6b866a81e6c372b022c699bbdb3a202fa87a
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-02 21:56:49 +0530

    added: snake-ladder-uc3

---
+++ snake_ladder_uc3.java
@@ -0,0 +1,33 @@
+package snake_ladder;
+
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class snake_ladder_uc3 {
+	
+		public static void main(String[] args) {
+			
+			int playerPosition = 0;
+			int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			System.out.println("Dice roll: " + die);
+			
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (option) {
+				case 0:
+					System.out.println("No play");
+					playerPosition += 0;
+					break;
+				case 1:
+					System.out.println("Ladder");
+					playerPosition += die;
+					break;
+				default:
+					System.out.println("Snake");
+					playerPosition -= die;
+					if (playerPosition < 0)
+						playerPosition = 0;		
+			}
+			System.out.println("Player position: " + playerPosition);
+		}
+	}
