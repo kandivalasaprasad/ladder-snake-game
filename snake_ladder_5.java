c4137d21d6dd5b09b39bb520d735e63e4118e4a2
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-02 21:59:31 +0530

    added: snake-ladder-uc5

---
+++ snake_ladder_5.java
@@ -0,0 +1,47 @@
+package snake_ladder;
+
+import java.util.Scanner;
+
+public class snake_ladder_5 {
+
+	 static final int maxPosition = 100;
+		public static void main(String[] args) {
+			System.out.print("Enter player name: ");
+			Scanner s = new Scanner(System.in);
+			String playerName = s.next();
+			int playerPosition = 0;
+			System.out.println("Player name: " + playerName);
+			
+			while (maxPosition > playerPosition ) {
+			int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			
+			int remainingPosition = maxPosition - playerPosition;
+			
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+
+			if( remainingPosition >= die ) {
+			switch (option) {
+				case 0:
+					System.out.println("No play : " + 0);
+					playerPosition += 0;
+					break;
+				case 1:
+					System.out.println("Ladder  : " + "+" + die);
+					playerPosition += die;
+					break;
+				default:
+					System.out.println("Snake   : " + "-" + die);
+					playerPosition -= die;
+					if (playerPosition < 0)
+						playerPosition = 0;
+			}
+
+			} // end of if
+
+			}
+			System.out.println("Player position: " + playerPosition);
+		}
+	}
+	
+
+
